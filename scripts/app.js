13555a84b16a9c87d4684c9109960cd30ab541e6
Felipe Martinez <f.martinez2@uniandes.edu.co>
2017-08-17 11:34:47 -0500

    F1: Agregue el código necesario para inyectar datos en el primer reload del cliente

--- scripts/app.js
+++ scripts/app.js
@@ -168,6 +168,10 @@
 
     };
 
+    var initialWeatherForecast = {
+        key: 'metros/1/bastille/A',
+        label: 'Bastille, Direction La Défense',
+    };
     // TODO uncomment line below to test app with fake data
     // app.updateForecastCard(initialWeatherForecast);
 
@@ -182,35 +186,29 @@
      *   SimpleDB (https://gist.github.com/inexorabletash/c8069c042b734519680c)
      ************************************************************************/
 
-    // TODO add startup code here
-    /*app.selectedCities = localStorage.selectedCities;
-     if (app.selectedCities) {
-     app.selectedCities = JSON.parse(app.selectedCities);
-     app.selectedCities.forEach(function(city) {
-     app.getForecast(city.key, city.label);
-     });
-     } else {
-     /!* The user is using the app for the first time, or the user has not
-     * saved any cities, so show the user some fake data. A real app in this
-     * scenario could guess the user's location via IP lookup and then inject
-     * that data into the page.
-     *!/
-     app.updateForecastCard(initialWeatherForecast);
-     app.selectedCities = [
-     {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
-     ];
-     app.saveSelectedCities();
-     }
-
+    app.selectedCities = localStorage.selectedCities;
+    if (app.selectedCities) {
+        app.selectedCities = JSON.parse(app.selectedCities);
+        app.selectedCities.forEach(function (city) {
+            app.getForecast(city.key, city.label);
+        });
+    } else {
+        /* The user is using the app for the first time, or the user has not
+        * saved any cities, so show the user some fake data. A real app in this
+        * scenario could guess the user's location via IP lookup and then inject
+        * that data into the page.
+        */
+        app.getSchedule(initialWeatherForecast.key, initialWeatherForecast.label);
+        app.selectedCities = [
+            {key: initialWeatherForecast.key, label: initialWeatherForecast.label}
+        ];
+        app.saveSelectedCities();
+    }
+    /*
      // TODO add service worker code here
      if ('serviceWorker' in navigator) {
      navigator.serviceWorker
      .register('./service-worker.js')
      .then(function() { console.log('Service Worker Registered'); });
      }*/
-
-    app.getSchedule('metros/1/bastille/A', 'Bastille, Direction La Défense');
-    app.selectedTimetables = [
-        {key: initialStationTimetable.key, label: initialStationTimetable.label}
-    ];
 })();
